5076e45c2b4fb5fc411e49d1bfab35b0efd5739a
antonellanieto <antonieca96@gmail.com>
2023-08-11 15:55:46 -0300

    folders

---
+++ ETL/cardano_date.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv("cardano_data.csv")
+
+# Convert Unix timestamps to datetime objects
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
+
+df_bitcoin = df.copy()
+
+df_bitcoin['Date'] = df['Timestamp']
+
+df_bitcoin.to_csv('cardano_date.csv', index=False)
--- ETL/read_tether.py
+++ ETL/read_tether.py
@@ -1,27 +1,27 @@
 import requests
 import csv
 
-# API endpoint URL
+
 url = 'https://api.coingecko.com/api/v3/coins/tether/market_chart/range?vs_currency=usd&from=1420081200&to=1672542000'
 
 
 
-# Parameters for the request
+
 params = {
     "vs_currency": "usd",
     "from": 1420081200,  # Unix timestamp for the "from" date
     "to": 1672542000    # Unix timestamp for the "to" date
 }
 
-# Make the API request
+
 response = requests.get(url, params=params)
 
-# Check if the request was successful
+
 if response.status_code == 200:
     data = response.json()
     prices = data["prices"]
     
-    # Write data to a CSV file
+
     with open("tether_data.csv", mode="w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(["Timestamp", "Price"])
